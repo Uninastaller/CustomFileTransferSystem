66267446d515facc618f21710a6e35c24ffdf58f
LizakT <tomas49226@gmail.com>
2023-06-17 19:05:12 +0200

    Refactor ssl server bussines logic

--- Modeel/SSL/SslServerBussinesLogic.cs
+++ Modeel/SSL/SslServerBussinesLogic.cs
@@ -14,22 +14,38 @@ using Timer = System.Timers.Timer;
 
 namespace Modeel.SSL
 {
-
     public class SslServerBussinesLogic : SslServer, IUniversalServerSocket
     {
-        private IWindowEnqueuer? _gui;
-        private Stopwatch? _stopwatch = new Stopwatch();
-        private Dictionary<Guid, string>? _clients = new Dictionary<Guid, string>();
+
+        #region Properties
+
         public TypeOfSocket Type { get; }
         public string TransferSendRateFormatedAsText { get; private set; } = string.Empty;
         public string TransferReceiveRateFormatedAsText { get; private set; } = string.Empty;
 
+        #endregion Properties
+
+        #region PublicFields
+
+
+
+        #endregion PublicFields
+
+        #region PrivateFields
+
+        private IWindowEnqueuer? _gui;
+        private Stopwatch? _stopwatch = new Stopwatch();
+        private Dictionary<Guid, string>? _clients = new Dictionary<Guid, string>();
 
         private Timer? _timer;
         private ulong _timerCounter;
 
         private long _secondOldBytesSent;
         private long _secondOldBytesReceived;
+
+        #endregion PrivateFields
+
+        #region Ctor
 
         public SslServerBussinesLogic(SslContext context, IPAddress address, int port, IWindowEnqueuer gui, int optionAcceptorBacklog = 1024) : base(context, address, port, optionAcceptorBacklog)
         {
@@ -43,6 +59,62 @@ namespace Modeel.SSL
             _timer.Start();
         }
 
+        #endregion Ctor
+
+        #region PublicMethods
+
+
+
+        #endregion PublicMethods
+
+        #region PrivateMethods
+
+        private void ClientStateChange(SocketState socketState, string? client, Guid sessionId)
+        {
+            if (_clients == null) return;
+
+            if (socketState == SocketState.CONNECTED && !_clients.ContainsKey(sessionId) && client != null)
+            {
+                _clients.Add(sessionId, client);
+                Logger.WriteLog($"Client: {client}, connected to server ", LoggerInfo.tcpServer);
+            }
+            else if (socketState == SocketState.DISCONNECTED && _clients.ContainsKey(sessionId))
+            {
+                Logger.WriteLog($"Client: {_clients[sessionId]}, disconnected from server ", LoggerInfo.tcpServer);
+                _clients.Remove(sessionId);
+            }
+        }
+
+        private void SendFile(string filePath, SslSession session)
+        {
+            // Open the file for reading
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                // Choose an appropriate buffer size based on the file size and system resources
+                int bufferSize = ResourceInformer.CalculateBufferSize(fileStream.Length);
+                Logger.WriteLog($"Fille buffer choosed for: {bufferSize}", LoggerInfo.P2PSSL);
+
+                byte[] buffer = new byte[bufferSize];
+                int bytesRead;
+                while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    // Send the bytes read from the file over the network stream
+                    //SslSession session = FindSession(_clients.ElementAt(0).Key);
+                    session.Send(buffer, 0, bytesRead);
+                }
+            }
+        }
+
+        #endregion PrivateMethods
+
+        #region ProtectedMethods
+
+
+
+        #endregion ProtectedMethods
+
+        #region EventHandler
+
         private void OneSecondHandler(object? sender, ElapsedEventArgs e)
         {
             _timerCounter++;
@@ -52,6 +124,21 @@ namespace Modeel.SSL
             _secondOldBytesSent = BytesSent;
             _secondOldBytesReceived = BytesReceived;
         }
+
+        private void OnReceiveMessage(SslSession sesion, string message)
+        {
+            Logger.WriteLog($"Tcp server obtained a message: {message}, from: {sesion.Socket.RemoteEndPoint}", LoggerInfo.socketMessage);
+
+            _stopwatch?.Start();
+            SendFile("C:\\Users\\tomas\\Downloads\\The.Office.US.S05.Season.5.Complete.720p.NF.WEB.x264-maximersk [mrsktv]\\The.Office.US.S05E15.720p.NF.WEB.x264-MRSK.mkv", sesion);
+            _stopwatch?.Stop();
+            TimeSpan elapsedTime = _stopwatch != null ? _stopwatch.Elapsed : TimeSpan.Zero;
+            Logger.WriteLog($"File transfer completed in {elapsedTime.TotalSeconds} seconds.", LoggerInfo.P2PSSL);
+        }
+
+        #endregion EventHandler
+
+        #region OverridedMethods
 
         protected override void OnDispose()
         {
@@ -101,51 +188,7 @@ namespace Modeel.SSL
                 _gui.BaseMsgEnque(new ClientStateChangeMessage() { Clients = _clients });
         }
 
-        private void OnReceiveMessage(SslSession sesion, string message)
-        {
-            Logger.WriteLog($"Tcp server obtained a message: {message}, from: {sesion.Socket.RemoteEndPoint}", LoggerInfo.socketMessage);
+        #endregion OverridedMethods
 
-            _stopwatch?.Start();
-            SendFile("C:\\Users\\tomas\\Downloads\\The.Office.US.S05.Season.5.Complete.720p.NF.WEB.x264-maximersk [mrsktv]\\The.Office.US.S05E15.720p.NF.WEB.x264-MRSK.mkv", sesion);
-            _stopwatch?.Stop();
-            TimeSpan elapsedTime = _stopwatch != null ? _stopwatch.Elapsed : TimeSpan.Zero;
-            Logger.WriteLog($"File transfer completed in {elapsedTime.TotalSeconds} seconds.", LoggerInfo.P2PSSL);
-        }
-
-        private void ClientStateChange(SocketState socketState, string? client, Guid sessionId)
-        {
-            if (_clients == null) return;
-
-            if (socketState == SocketState.CONNECTED && !_clients.ContainsKey(sessionId) && client != null)
-            {
-                _clients.Add(sessionId, client);
-                Logger.WriteLog($"Client: {client}, connected to server ", LoggerInfo.tcpServer);
-            }
-            else if (socketState == SocketState.DISCONNECTED && _clients.ContainsKey(sessionId))
-            {
-                Logger.WriteLog($"Client: {_clients[sessionId]}, disconnected from server ", LoggerInfo.tcpServer);
-                _clients.Remove(sessionId);
-            }
-        }
-
-        private void SendFile(string filePath, SslSession session)
-        {
-            // Open the file for reading
-            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-            {
-                // Choose an appropriate buffer size based on the file size and system resources
-                int bufferSize = ResourceInformer.CalculateBufferSize(fileStream.Length);
-                Logger.WriteLog($"Fille buffer choosed for: {bufferSize}", LoggerInfo.P2PSSL);
-
-                byte[] buffer = new byte[bufferSize];
-                int bytesRead;
-                while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    // Send the bytes read from the file over the network stream
-                    //SslSession session = FindSession(_clients.ElementAt(0).Key);
-                    session.Send(buffer, 0, bytesRead);
-                }
-            }
-        }
     }
 }
