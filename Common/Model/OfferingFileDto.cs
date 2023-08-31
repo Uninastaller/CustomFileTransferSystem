2b48c11a556b5f6383a34bc62ef202407030e99b
Tomas LIZAK <Tomas.LIZAK@frequentis.com>
2023-08-31 12:21:19 +0200

    Immplementing receiving offer files in central server, saving, merging. Also changed OfferingFilesDto list of endpoint to dictionary witch grades for every endpoint

--- Common/Model/OfferingFileDto.cs
+++ Common/Model/OfferingFileDto.cs
@@ -1,15 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Common.Model
 {
-    public class OfferingFileDto
-    {
-        public string FileName { get; set; } = string.Empty;
-        public long FileSize { get; set; }
-        public List<string> IpAddressesAndPorts { get; set; } = new List<string>();
-    }
+   public class OfferingFileDto
+   {
+      public string FileName { get; set; } = string.Empty;
+      public long FileSize { get; set; }
+      /// <summary>
+      /// Key: ipaddress:port
+      /// Value: grade
+      /// </summary>
+      public Dictionary<string, int> EndpointsAndGrades { get; set; } = new Dictionary<string, int>();
+
+      public OfferingFileDto()
+      {
+
+      }
+
+      public OfferingFileDto(string EndPoint)
+      {
+         EndpointsAndGrades.Add(EndPoint, 0);
+      }
+
+      public void MergeWithAnotherOfferingFileDto(OfferingFileDto offeringFileDto)
+      {
+         foreach (KeyValuePair<string, int> keyValuePair in offeringFileDto.EndpointsAndGrades)
+         {
+            this.EndpointsAndGrades[keyValuePair.Key] = 0;            
+         }
+
+         //this.EndpointsAndGrades.Keys.Intersect(offeringFileDto.EndpointsAndGrades.Keys).ToList().ForEach(key => this.EndpointsAndGrades[key] = 0);
+      }
+   }
 }
--- Common/Model/ResourceInformer.cs
+++ Common/Model/ResourceInformer.cs
@@ -1,181 +1,179 @@
-﻿using Common.Enum;
-using Common.Interface;
+﻿using Common.Interface;
 using ConfigManager;
 using Logger;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Text;
-using System.Xml;
+using System.Threading.Tasks;
 
 namespace Common.Model
 {
-    public static class ResourceInformer
-    {
+   public static class ResourceInformer
+   {
 
-        #region Properties
-
-
-
-        #endregion Properties
-
-        #region PublicFields
-
-        #endregion PublicFields
-
-        #region PrivateFields
-
-        private const int _kilobyte = 0x400;
-        private const int _megabyte = 0x100000;
-        private const string _cftsDirectoryName = "CFTS";
-        private const string _cftsFileExtensions = ".cfts";
-
-        #endregion PrivateFields
-
-        #region ProtectedFields
+      #region Properties
 
 
 
-        #endregion ProtectedFields
+      #endregion Properties
 
-        #region Ctor
+      #region PublicFields
+
+      public const char offeringFilesJoint = '_';
+
+      #endregion PublicFields
+
+      #region PrivateFields
+
+      private const int _kilobyte = 0x400;
+      private const int _megabyte = 0x100000;
+      private const string _cftsDirectoryName = "CFTS";
+      private const string _cftsFileExtensions = ".cfts";
+
+      #endregion PrivateFields
+
+      #region ProtectedFields
 
 
 
-        #endregion Ctor
+      #endregion ProtectedFields
 
-        #region PublicMethods
+      #region Ctor
 
-        public static string FormatDataTransferRate(long bytesSent)
-        {
-            string unit;
-            double transferRate;
 
-            if (bytesSent < _kilobyte)
-            {
-                transferRate = bytesSent;
-                unit = "B/s";
-            }
-            else if (bytesSent < _megabyte)
-            {
-                transferRate = (double)bytesSent / _kilobyte;
-                unit = "KB/s";
-            }
-            else
-            {
-                transferRate = (double)bytesSent / _megabyte;
-                unit = "MB/s";
-            }
 
-            return $"{transferRate:F2} {unit}";
-        }
+      #endregion Ctor
 
-        public static void LoadCftsJsonsAndSendToSession(string uploadingDirectoryPath, ISession session)
-        {
-            string cftsFilesDirectory = Path.Combine(uploadingDirectoryPath, _cftsDirectoryName);
-            if (Directory.Exists(cftsFilesDirectory))
-            {
-                string[] files = Directory.GetFiles(cftsFilesDirectory);
+      #region PublicMethods
 
-                foreach (string filePath in files)
-                {
-                    if (Path.GetExtension(filePath).Equals(_cftsFileExtensions))
-                    {
-                        // Read content of file
-                        string jsonString = File.ReadAllText(filePath);
-                        Log.WriteLog(LogLevel.INFO, $"Reading content of file: {filePath}, content: {jsonString.Replace('\n', ' ').Replace('\r', ' ')}");
-                        // Validate if conten is valid json
-                        try
-                        {
-                            // Attempt to parse the JSON string
-                            OfferingFileDto? offeringFileDto = JsonConvert.DeserializeObject<OfferingFileDto>(jsonString);
+      public static string FormatDataTransferRate(long bytesSent)
+      {
+         string unit;
+         double transferRate;
 
-                            // If parsing succeeds, the JSON is valid
-                            Log.WriteLog(LogLevel.INFO, "Content is valid");
-                            FlagMessagesGenerator.GenerateOfferingFile(jsonString, session);
-                        }
-                        catch (JsonException ex)
-                        {
-                            // Parsing failed, so the JSON is not valid
-                            Log.WriteLog(LogLevel.WARNING, "Content is invalid! " + ex.Message);
-                        }
-                    }
-                }
-            }
-        }
+         if (bytesSent < _kilobyte)
+         {
+            transferRate = bytesSent;
+            unit = "B/s";
+         }
+         else if (bytesSent < _megabyte)
+         {
+            transferRate = (double)bytesSent / _kilobyte;
+            unit = "KB/s";
+         }
+         else
+         {
+            transferRate = (double)bytesSent / _megabyte;
+            unit = "MB/s";
+         }
 
-        public static void OnUploadFileRequest(IPAddress ipAddress, int port, ISession session) => OnUploadFileRequest(ipAddress.ToString(), port, session);
+         return $"{transferRate:F2} {unit}";
+      }
 
-        public static void OnUploadFileRequest(string ipAddress, int port, ISession session)
-        {
-            string UploadingDirectoryPath = MyConfigManager.GetConfigValue("UploadingDirectory");
-            CreateJsonFiles(ipAddress, port, UploadingDirectoryPath);
-            LoadCftsJsonsAndSendToSession(UploadingDirectoryPath, session);
-        }
-
-        public static void CreateJsonFiles(IPAddress ipAddress, int port, string uploadingDirectoryPath) => CreateJsonFiles(ipAddress.ToString(), port, uploadingDirectoryPath);
-
-        public static void CreateJsonFiles(string ipAddress, int port, string uploadingDirectoryPath)
-        {
-            string directoryPathToCftsDirectory = Path.Combine(uploadingDirectoryPath, _cftsDirectoryName);
-            if (!Directory.Exists(directoryPathToCftsDirectory))
-            {
-                Directory.CreateDirectory(directoryPathToCftsDirectory);
-            }
-
-            string[] files = Directory.GetFiles(uploadingDirectoryPath);
+      public static void LoadCftsJsonsAndSendToSession(string uploadingDirectoryPath, ISession session)
+      {
+         string cftsFilesDirectory = Path.Combine(uploadingDirectoryPath, _cftsDirectoryName);
+         if (Directory.Exists(cftsFilesDirectory))
+         {
+            string[] files = Directory.GetFiles(cftsFilesDirectory);
 
             foreach (string filePath in files)
             {
-                FileInfo fileInfo = new FileInfo(filePath);
-                CreateJsonFile(ipAddress, port, fileInfo);
+               if (Path.GetExtension(filePath).Equals(_cftsFileExtensions))
+               {
+                  // Read content of file
+                  string jsonString = File.ReadAllText(filePath);
+                  Log.WriteLog(LogLevel.INFO, $"Reading content of file: {filePath}, content: {jsonString.Replace('\n', ' ').Replace('\r', ' ')}");
+                  // Validate if conten is valid json
+                  try
+                  {
+                     // Attempt to parse the JSON string
+                     OfferingFileDto? offeringFileDto = JsonConvert.DeserializeObject<OfferingFileDto>(jsonString);
+
+                     // If parsing succeeds, the JSON is valid
+                     Log.WriteLog(LogLevel.INFO, "Content is valid");
+                     FlagMessagesGenerator.GenerateOfferingFile(jsonString, session);
+                  }
+                  catch (JsonException ex)
+                  {
+                     // Parsing failed, so the JSON is not valid
+                     Log.WriteLog(LogLevel.WARNING, "Content is invalid! " + ex.Message);
+                  }
+               }
             }
-            Log.WriteLog(LogLevel.INFO, "Created .ctfs files in director: " + directoryPathToCftsDirectory);
-        }
+         }
+      }
 
-        public static void CreateJsonFile(string ipAddress, int port, FileInfo fileInfo)
-        {
-            var fileInfoDto = new OfferingFileDto
-            {
-                FileName = fileInfo.Name,
-                FileSize = fileInfo.Length,
-                IpAddressesAndPorts = new List<string> { $"{ipAddress}:{port}" }
-            };
+      public static void OnUploadFileRequest(IPAddress ipAddress, int port, ISession session) => OnUploadFileRequest(ipAddress.ToString(), port, session);
 
-            string json = JsonConvert.SerializeObject(fileInfoDto, Newtonsoft.Json.Formatting.Indented);
-            string jsonFileName = $"{Path.GetFileNameWithoutExtension(fileInfo.Name)}_{fileInfo.Length}{_cftsFileExtensions}";
-            string jsonFilePath = Path.Combine(fileInfo.DirectoryName, _cftsDirectoryName, jsonFileName);
+      public static void OnUploadFileRequest(string ipAddress, int port, ISession session)
+      {
+         string UploadingDirectoryPath = MyConfigManager.GetConfigValue("UploadingDirectory");
+         CreateJsonFiles(ipAddress, port, UploadingDirectoryPath);
+         LoadCftsJsonsAndSendToSession(UploadingDirectoryPath, session);
+      }
 
-            File.WriteAllText(jsonFilePath, json);
-        }
+      public static void CreateJsonFiles(IPAddress ipAddress, int port, string uploadingDirectoryPath) => CreateJsonFiles(ipAddress.ToString(), port, uploadingDirectoryPath);
 
-        #endregion PublicMethods
+      public static void CreateJsonFiles(string ipAddress, int port, string uploadingDirectoryPath)
+      {
+         string directoryPathToCftsDirectory = Path.Combine(uploadingDirectoryPath, _cftsDirectoryName);
+         if (!Directory.Exists(directoryPathToCftsDirectory))
+         {
+            Directory.CreateDirectory(directoryPathToCftsDirectory);
+         }
 
-        #region PrivateMethods
+         string[] files = Directory.GetFiles(uploadingDirectoryPath);
+
+         foreach (string filePath in files)
+         {
+            FileInfo fileInfo = new FileInfo(filePath);
+            CreateJsonFile(ipAddress, port, fileInfo);
+         }
+         Log.WriteLog(LogLevel.INFO, "Created .ctfs files in director: " + directoryPathToCftsDirectory);
+      }
+
+      public static void CreateJsonFile(string ipAddress, int port, FileInfo fileInfo)
+      {
+         var offeringFileDto = new OfferingFileDto($"{ipAddress}:{port}")
+         {
+            FileName = fileInfo.Name,
+            FileSize = fileInfo.Length,
+         };
+
+         string json = JsonConvert.SerializeObject(offeringFileDto, Newtonsoft.Json.Formatting.Indented);
+         string jsonFileName = $"{Path.GetFileNameWithoutExtension(fileInfo.Name)}{offeringFilesJoint}{fileInfo.Length}{_cftsFileExtensions}";
+         string jsonFilePath = Path.Combine(fileInfo.DirectoryName, _cftsDirectoryName, jsonFileName);
+
+         File.WriteAllText(jsonFilePath, json);
+      }
+
+      #endregion PublicMethods
+
+      #region PrivateMethods
 
 
 
-        #endregion PrivateMethods
+      #endregion PrivateMethods
 
-        #region ProtectedMethods
-
-
-
-        #endregion ProtectedMethods
-
-        #region Events
+      #region ProtectedMethods
 
 
 
-        #endregion Events
+      #endregion ProtectedMethods
 
-        #region OverridedMethods
+      #region Events
 
 
 
-        #endregion OverridedMethods
+      #endregion Events
 
-    }
+      #region OverridedMethods
+
+
+
+      #endregion OverridedMethods
+
+   }
 }
