b3b554bb2d4aa3f47ec52325ce0b158be1039626
Tomas LIZAK <Tomas.LIZAK@frequentis.com>
2023-09-04 14:47:10 +0200

    All image buttons styles changed to use just one image and automatically change its color by image mask

--- Client/Windows/MainWindow.xaml.cs
+++ Client/Windows/MainWindow.xaml.cs
@@ -18,231 +18,253 @@ using System.Windows.Media;
 
 namespace Client.Windows
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : BaseWindowForWPF
-    {
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : BaseWindowForWPF
+   {
 
-        #region Properties
+      #region Properties
 
-        //public ClientSocketState ConnectionWithCentralServerSocketState
-        //{
-        //    get => _connectionWithCentralServerSocketState;
-        //    set
-        //    {
-        //        _connectionWithCentralServerSocketState = value;
-        //        Log.WriteLog(LogLevel.INFO, "Connection With Central Server Socekt State Change To " + value);
+      //public ClientSocketState ConnectionWithCentralServerSocketState
+      //{
+      //    get => _connectionWithCentralServerSocketState;
+      //    set
+      //    {
+      //        _connectionWithCentralServerSocketState = value;
+      //        Log.WriteLog(LogLevel.INFO, "Connection With Central Server Socekt State Change To " + value);
 
-        //        switch (value)
-        //        {
-        //            case ClientSocketState.CONNECTED:
-        //                elpServerStatus.Fill = new SolidColorBrush(Color.FromArgb(0xFF, 0x26, 0x3F, 0x03)); // Using Color class
-        //                break;
-        //            case ClientSocketState.DISCONNECTED:
-        //                elpServerStatus.Fill = new SolidColorBrush(Color.FromArgb(0xFF, 0x74, 0x1B, 0x0C)); // Using Color class
-        //                break;
-        //            default:
-        //                break;
-        //        }
-        //    }
-        //}
+      //        switch (value)
+      //        {
+      //            case ClientSocketState.CONNECTED:
+      //                elpServerStatus.Fill = new SolidColorBrush(Color.FromArgb(0xFF, 0x26, 0x3F, 0x03)); // Using Color class
+      //                break;
+      //            case ClientSocketState.DISCONNECTED:
+      //                elpServerStatus.Fill = new SolidColorBrush(Color.FromArgb(0xFF, 0x74, 0x1B, 0x0C)); // Using Color class
+      //                break;
+      //            default:
+      //                break;
+      //        }
+      //    }
+      //}
 
-        #endregion Properties
+      #endregion Properties
 
-        #region PublicFields
-
-
-
-        #endregion PublicFields
-
-        #region PrivateFields
-
-        //private IUniversalClientSocket? _socketToCentralServer;
-        private readonly string _certificateNameForCentralServerConnect = "MyTestCertificateClient.pfx";
-        private readonly SslContext _contextForCentralServerConnect;
-        private IPAddress _centralServerIpAddress = NetworkUtils.GetLocalIPAddress() ?? IPAddress.Loopback;
-        private int _centralServerPort = 34258;
-        //private ClientSocketState _connectionWithCentralServerSocketState = ClientSocketState.DISCONNECTED;
-
-        private readonly ObservableCollection<OfferingFileDto> _offeringFiles = new ObservableCollection<OfferingFileDto>();
-
-        #endregion PrivateFields
-
-        #region ProtectedFields
+      #region PublicFields
 
 
 
-        #endregion ProtectedFields
+      #endregion PublicFields
 
-        #region Ctor
+      #region PrivateFields
 
-        public MainWindow()
-        {
-            InitializeComponent();
+      //private IUniversalClientSocket? _socketToCentralServer;
+      private readonly string _certificateNameForCentralServerConnect = "MyTestCertificateClient.pfx";
+      private readonly SslContext _contextForCentralServerConnect;
+      private IPAddress _centralServerIpAddress = NetworkUtils.GetLocalIPAddress() ?? IPAddress.Loopback;
+      private int _centralServerPort = 34258;
+      //private ClientSocketState _connectionWithCentralServerSocketState = ClientSocketState.DISCONNECTED;
 
-            if (MyConfigManager.TryGetBoolConfigValue("EnableDynamicGradients", out bool enableDynamicGradients) && enableDynamicGradients)
+      private readonly ObservableCollection<OfferingFileDto> _offeringFiles = new ObservableCollection<OfferingFileDto>();
+
+      #endregion PrivateFields
+
+      #region ProtectedFields
+
+
+
+      #endregion ProtectedFields
+
+      #region Ctor
+
+      public MainWindow()
+      {
+         InitializeComponent();
+
+         if (MyConfigManager.TryGetBoolConfigValue("EnableDynamicGradients", out bool enableDynamicGradients) && enableDynamicGradients)
+         {
+            WindowDesignSet();
+         }
+
+         contract.Add(MsgIds.ClientSocketStateChangeMessage, typeof(ClientSocketStateChangeMessage));
+         contract.Add(MsgIds.OfferingFilesReceivedMessage, typeof(OfferingFilesReceivedMessage));
+
+         if (MyConfigManager.TryGetConfigValue<Int32>("CeentralServerPort", out Int32 centralServerPort))
+         {
+            _centralServerPort = centralServerPort;
+         }
+
+         _contextForCentralServerConnect = new SslContext(SslProtocols.Tls12, new X509Certificate2(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _certificateNameForCentralServerConnect), ""), (sender, certificate, chain, sslPolicyErrors) => true);
+
+         Init();
+      }
+
+      #endregion Ctor
+
+      #region PublicMethods
+
+
+
+      #endregion PublicMethods
+
+      #region PrivateMethods
+
+      private void Init()
+      {
+
+         msgSwitch
+          .Case(contract.GetContractId(typeof(ClientSocketStateChangeMessage)), (ClientSocketStateChangeMessage x) => ClientSocketStateChangeMessageHandler(x))
+          .Case(contract.GetContractId(typeof(OfferingFilesReceivedMessage)), (OfferingFilesReceivedMessage x) => OfferingFilesReceivedMessageHandler(x.OfferingFiles))
+          ;
+
+         tbTitle.Text = $"Custom File Transfer System [v.{Assembly.GetExecutingAssembly().GetName().Version}]";
+
+         //new SslClientBussinesLogic(_contextForCentralServerConnect, _centralServerIpAddress, _centralServerPort, this,
+         //    typeOfSession: TypeOfSession.UPDATING_OFFERING_FILES_SESSION_WITH_CENTRAL_SERVER, optionReceiveBufferSize: 0x2000, optionSendBufferSize: 0x2000);
+
+         dtgOfferingFiles.ItemsSource = _offeringFiles;
+      }
+
+      private void WindowDesignSet()
+      {
+         // Generate random points
+         Random rand = new Random();
+         double startX = rand.NextDouble();
+         double startY = rand.NextDouble();
+         double endX = rand.NextDouble();
+         double endY = rand.NextDouble();
+
+         // Generate random offsets
+         double[] randomOffsets = new double[4] { rand.NextDouble(), rand.NextDouble(), rand.NextDouble(), rand.NextDouble() };
+
+         // Sort them to ensure they are in ascending order
+         Array.Sort(randomOffsets);
+
+         // Create new LinearGradientBrush
+         LinearGradientBrush newBrush = new LinearGradientBrush()
+         {
+            StartPoint = new Point(startX, startY),
+            EndPoint = new Point(endX, endY),
+         };
+
+         // Add GradientStops to the LinearGradientBrush
+         newBrush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#4e3926"), randomOffsets[0]));
+         newBrush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#453a26"), randomOffsets[1]));
+         newBrush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#753b22"), randomOffsets[2]));
+         newBrush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#383838"), randomOffsets[3]));
+
+         brdSecond.BorderBrush = newBrush;
+         gdMain.Background = newBrush;
+      }
+
+      private void ClientSocketStateChangeMessageHandler(ClientSocketStateChangeMessage message)
+      {
+         Log.WriteLog(LogLevel.DEBUG, "ClientSocketStateChangeMessageHandler");
+         if (message.ClientSocketState == ClientSocketState.CONNECTED)
+         {
+            if (message.TypeOfSession == TypeOfSession.DOWNLOADING_OFFERING_FILES_SESSION_WITH_CENTRAL_SERVER)
             {
-                WindowDesignSet();
+               _offeringFiles.Clear();
+               btnDownloadOfferingFilesFromCetnralServer.IsEnabled = true;
             }
 
-            contract.Add(MsgIds.ClientSocketStateChangeMessage, typeof(ClientSocketStateChangeMessage));
-            contract.Add(MsgIds.OfferingFilesReceivedMessage, typeof(OfferingFilesReceivedMessage));
-
-            if (MyConfigManager.TryGetConfigValue<Int32>("CeentralServerPort", out Int32 centralServerPort))
+         }
+         else if (message.ClientSocketState == ClientSocketState.DISCONNECTED)
+         {
+            if (message.TypeOfSession == TypeOfSession.UPDATING_OFFERING_FILES_SESSION_WITH_CENTRAL_SERVER)
             {
-                _centralServerPort = centralServerPort;
+               btnUploadOfferingFileToCentralServer.IsEnabled = true;
             }
+         }
+      }
 
-            _contextForCentralServerConnect = new SslContext(SslProtocols.Tls12, new X509Certificate2(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _certificateNameForCentralServerConnect), ""), (sender, certificate, chain, sslPolicyErrors) => true);
+      private void OfferingFilesReceivedMessageHandler(List<OfferingFileDto> offeringFiles)
+      {
+         Log.WriteLog(LogLevel.DEBUG, "OfferingFilesReceivedMessageHandler");
+         foreach (var offeringFile in offeringFiles)
+         {
+            _offeringFiles.Add(offeringFile);
+         }
+      }
 
-            Init();
-        }
+      #endregion PrivateMethods
 
-        #endregion Ctor
-
-        #region PublicMethods
-
-
-
-        #endregion PublicMethods
-
-        #region PrivateMethods
-
-        private void Init()
-        {
-
-            msgSwitch
-             .Case(contract.GetContractId(typeof(ClientSocketStateChangeMessage)), (ClientSocketStateChangeMessage x) => ClientSocketStateChangeMessageHandler(x))
-             .Case(contract.GetContractId(typeof(OfferingFilesReceivedMessage)), (OfferingFilesReceivedMessage x) => OfferingFilesReceivedMessageHandler(x.OfferingFiles))
-             ;
-
-            tbTitle.Text = $"Custom File Transfer System [v.{Assembly.GetExecutingAssembly().GetName().Version}]";
-
-            //new SslClientBussinesLogic(_contextForCentralServerConnect, _centralServerIpAddress, _centralServerPort, this,
-            //    typeOfSession: TypeOfSession.UPDATING_OFFERING_FILES_SESSION_WITH_CENTRAL_SERVER, optionReceiveBufferSize: 0x2000, optionSendBufferSize: 0x2000);
-
-            dtgOfferingFiles.ItemsSource = _offeringFiles;
-        }
-
-        private void WindowDesignSet()
-        {
-            // Generate random points
-            Random rand = new Random();
-            double startX = rand.NextDouble();
-            double startY = rand.NextDouble();
-            double endX = rand.NextDouble();
-            double endY = rand.NextDouble();
-
-            // Generate random offsets
-            double[] randomOffsets = new double[4] { rand.NextDouble(), rand.NextDouble(), rand.NextDouble(), rand.NextDouble() };
-
-            // Sort them to ensure they are in ascending order
-            Array.Sort(randomOffsets);
-
-            // Create new LinearGradientBrush
-            LinearGradientBrush newBrush = new LinearGradientBrush()
-            {
-                StartPoint = new Point(startX, startY),
-                EndPoint = new Point(endX, endY),
-            };
-
-            // Add GradientStops to the LinearGradientBrush
-            newBrush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#4e3926"), randomOffsets[0]));
-            newBrush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#453a26"), randomOffsets[1]));
-            newBrush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#753b22"), randomOffsets[2]));
-            newBrush.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#383838"), randomOffsets[3]));
-
-            brdSecond.BorderBrush = newBrush;
-            gdMain.Background = newBrush;
-        }
-
-        private void ClientSocketStateChangeMessageHandler(ClientSocketStateChangeMessage message)
-        {
-            Log.WriteLog(LogLevel.DEBUG, "ClientSocketStateChangeMessageHandler");
-            if (message.ClientSocketState == ClientSocketState.CONNECTED)
-            {
-                if (message.TypeOfSession == TypeOfSession.DOWNLOADING_OFFERING_FILES_SESSION_WITH_CENTRAL_SERVER)
-                {
-                    _offeringFiles.Clear();
-                }
-            }
-        }
-
-        private void OfferingFilesReceivedMessageHandler(List<OfferingFileDto> offeringFiles)
-        {
-            Log.WriteLog(LogLevel.DEBUG, "OfferingFilesReceivedMessageHandler");
-            foreach (var offeringFile in offeringFiles)
-            {
-                _offeringFiles.Add(offeringFile);
-            }
-        }
-
-        #endregion PrivateMethods
-
-        #region ProtectedMethods
+      #region ProtectedMethods
 
 
 
-        #endregion ProtectedMethods
+      #endregion ProtectedMethods
 
-        #region Events
+      #region Events
 
-        private void Border_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            this.DragMove();
-        }
+      private void Border_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+      {
+         this.DragMove();
+      }
 
-        private void btnClose_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            this.Close();
-            Environment.Exit(0);
-        }
-
-
-        private void btnMinimize_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            this.WindowState = System.Windows.WindowState.Minimized;
-        }
-
-        private void btnMaximize_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            if (this.WindowState == System.Windows.WindowState.Maximized)
-            {
-                this.WindowState = System.Windows.WindowState.Normal; // Restore window size
-            }
-            else
-            {
-                this.WindowState = System.Windows.WindowState.Maximized; // Maximize window
-            }
-        }
+      private void btnClose_Click(object sender, System.Windows.RoutedEventArgs e)
+      {
+         this.Close();
+         Environment.Exit(0);
+      }
 
 
-        private void btnDownloadOfferingFile_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button)
-            {
-               Log.WriteLog(LogLevel.DEBUG, button.Name);          
-            }
-        }
+      private void btnMinimize_Click(object sender, System.Windows.RoutedEventArgs e)
+      {
+         this.WindowState = System.Windows.WindowState.Minimized;
+      }
 
-        private void btnOfferingFilesWindow_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button)
-            {
-                Log.WriteLog(LogLevel.DEBUG, button.Name);
-                new SslClientBussinesLogic(_contextForCentralServerConnect, _centralServerIpAddress, _centralServerPort, this,
-                    typeOfSession: TypeOfSession.DOWNLOADING_OFFERING_FILES_SESSION_WITH_CENTRAL_SERVER, optionReceiveBufferSize: 0x2000, optionSendBufferSize: 0x2000);
-            }
-        }
-
-        #endregion Events
-
-        #region OverridedMethods
+      private void btnMaximize_Click(object sender, System.Windows.RoutedEventArgs e)
+      {
+         if (this.WindowState == System.Windows.WindowState.Maximized)
+         {
+            this.WindowState = System.Windows.WindowState.Normal; // Restore window size
+         }
+         else
+         {
+            this.WindowState = System.Windows.WindowState.Maximized; // Maximize window
+         }
+      }
 
 
+      private void btnDownloadOfferingFile_Click(object sender, RoutedEventArgs e)
+      {
+         if (sender is Button button)
+         {
+            Log.WriteLog(LogLevel.DEBUG, button.Name);
+         }
+      }
 
-        #endregion OverridedMethods
+      private void btnDownloadOfferingFilesFromCetnralServer_Click(object sender, RoutedEventArgs e)
+      {
+         if (sender is Button button)
+         {
+            button.IsEnabled = false;
+            Log.WriteLog(LogLevel.DEBUG, button.Name);
+            new SslClientBussinesLogic(_contextForCentralServerConnect, _centralServerIpAddress, _centralServerPort, this,
+                typeOfSession: TypeOfSession.DOWNLOADING_OFFERING_FILES_SESSION_WITH_CENTRAL_SERVER, optionReceiveBufferSize: 0x2000, optionSendBufferSize: 0x2000);
+         }
+      }
 
-    }
+
+      private void btnUploadOfferingFileToCentralServer_Click(object sender, RoutedEventArgs e)
+      {
+         if (sender is Button button)
+         {
+            button.IsEnabled = false;
+            Log.WriteLog(LogLevel.DEBUG, button.Name);
+            new SslClientBussinesLogic(_contextForCentralServerConnect, _centralServerIpAddress, _centralServerPort, this,
+                typeOfSession: TypeOfSession.UPDATING_OFFERING_FILES_SESSION_WITH_CENTRAL_SERVER, optionReceiveBufferSize: 0x2000, optionSendBufferSize: 0x2000);
+         }
+      }
+
+      #endregion Events
+
+      #region OverridedMethods
+
+
+
+      #endregion OverridedMethods
+
+   }
 }
